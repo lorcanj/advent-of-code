4ce3778c7e3a3256ae3c8314049a92e1af2fe42f
Lorcan <lorcanjames1@gmail.com>
2024-12-29 14:04:53 +0000

    update code for part 2

--- day 10/part 1.py
+++ day 10/part 1.py
@@ -15,6 +15,7 @@ for i in range(len(island)):
     for j in range(len(island[i])):
         if island[i][j] == 0:
             starts.append((i, j))
+#print(starts)
 
 
 def in_bound(row, col, matrix) -> bool:
@@ -23,27 +24,29 @@ def in_bound(row, col, matrix) -> bool:
 
 def valid_path(row, col, matrix, visited: set, found: set) -> (int, tuple):
     if not in_bound(row, col, matrix): return 0, found
-    if (row, col) in visited: return 0, found
-    if matrix[row][col] == 9 and (row, col) not in found:
-        found.add((row, col))
+    #if (row, col) in visited: return 0, found
+    if matrix[row][col] == 9:
+        #and (row, col) not in found:
+        #found.add((row, col))
         return 1, found
     visited.add((row, col))
 
     dirs = [-1, 0, 1, 0, -1]
     is_valid = 0
     for index in range(len(dirs) - 1):
-        # below is wrong
         new_dir = (row + dirs[index], col + dirs[index + 1])
         if in_bound(new_dir[0], new_dir[1], matrix):
             if matrix[new_dir[0]][new_dir[1]] - matrix[row][col] == 1:
                 things = valid_path(new_dir[0], new_dir[1], matrix, visited, found)
                 is_valid += things[0]
-                found = things[1]
+                #found = things[1]
     return is_valid, found
 
 
 ans = 0
 for start in starts:
+    #print(f"{start[0]} and {start[1]}")
     thing = valid_path(start[0], start[1], island, set(), set())
     ans += thing[0]
-
+    print(thing)
+print(ans)
