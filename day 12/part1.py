ac925812b6bbbb5985c7f8ca802f7b133a80f30f
Lorcan <lorcanjames1@gmail.com>
2025-01-05 13:28:49 +0000

    ugly part 2

--- day 12/part1.py
+++ day 12/part1.py
@@ -4,6 +4,7 @@ test = "test.txt"
 test2 = "test2.txt"
 test3 = "test3.txt"
 input = "input.txt"
+test4 = "test4.txt"
 
 
 # area is just the number of letters in the island
@@ -47,6 +48,83 @@ def is_connected(point_a: tuple, point_b: tuple) -> bool:
     return connected or point_a[1] == point_b[1] and abs(point_a[0] - point_b[0]) == 1
 
 
+def count_corners(set_of_points: set[tuple]) -> int:
+    corners = 0
+    for point in set_of_points:
+
+        above = point[0] - 1, point[1]
+        right = point[0], point[1] + 1
+        below = point[0] + 1, point[1]
+        left = point[0], point[1] - 1
+        connected_points = {above, right, below, left}
+        connected = 0
+        for p in connected_points:
+            if p in set_of_points:
+                connected += 1
+        top_left = point[0] - 1, point[1] - 1
+        top_right = point[0] - 1, point[1] + 1
+        bottom_left = point[0] + 1, point[1] - 1
+        bottom_right = point[0] + 1, point[1] + 1
+        diagonal_points = {top_left, top_right, bottom_left, bottom_right}
+        # this wrong as well as internal corners
+        if connected == 4:
+            for diagonal in diagonal_points:
+                if diagonal not in set_of_points:
+                    corners += 1
+        # not necessarily + 2 as could be filled in
+        elif connected == 3:
+            # need to check the particular points
+            if above in set_of_points and below in set_of_points:
+                if left in set_of_points:
+                    if top_left not in set_of_points:
+                        corners += 1
+                    if bottom_left not in set_of_points:
+                        corners += 1
+                else:
+                    if top_right not in set_of_points:
+                        corners += 1
+                    if bottom_right not in set_of_points:
+                        corners += 1
+            else:
+                if above in set_of_points:
+                    if top_left not in set_of_points:
+                        corners += 1
+                    if top_right not in set_of_points:
+                        corners += 1
+                else:
+                    if bottom_right not in set_of_points:
+                        corners += 1
+                    if bottom_left not in set_of_points:
+                        corners += 1
+        elif connected == 1:
+            corners += 2
+        elif connected == 0:
+            corners += 4
+        else:
+            if (above in set_of_points and below in set_of_points) or (left in set_of_points and right in set_of_points):
+                continue
+            else:
+                # need to check for one more point
+                new_point = None
+                if above in set_of_points and right in set_of_points:
+                    new_point = above[0], right[1]
+                elif below in set_of_points and right in set_of_points:
+                    new_point = below[0], right[1]
+                elif below in set_of_points and left in set_of_points:
+                    new_point = below[0], left[1]
+                else:
+                    new_point = above[0], left[1]
+                if new_point in set_of_points:
+                    corners += 1
+                else:
+                      corners += 2
+    return corners
+
+
+
+
+
+
 # build input
 file = open(input)
 # island is a n x n matrix
@@ -66,14 +144,28 @@ for row in range(len(matrix)):
             letter = matrix[row][col]
             dfs_islands(row, col, matrix, visited, letter)
             actual = visited - before_set
-            separate_islands.append(list(actual))
+            #separate_islands.append(actual)
 
             # part 1 calc
             # # area is number of segments
-            # area = len(actual)
+            area = len(actual)
             #
             # # perimeter is more complicated
-            # perimeter = count_connections(actual)
-            # ans += (area * perimeter)
+            perimeter = count_corners(actual)
+            print(area)
+            print(perimeter)
+            ans += (area * perimeter)
+
+print(ans)
+
+# can just count corners for the number of lines
+# every line ends with a corner
+# so number of lines == number of corners
+# ans = 0
+#
+#
+# for island in separate_islands:
+#     ans += count_corners(island)
+# print(ans)
 
 
